ba080ab350c040a0055ed44cfbe68efb9c0468b6
destpat <yoan.ficadiere@gmail.com>
2016-05-03 19:11:54 +0200

    state go

--- www/js/app.js
+++ www/js/app.js
@@ -32,23 +32,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
   .state('app.compte', {
       url: '/compte',
       views: {
@@ -58,25 +41,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-    .state('app.playlists', {
-      url: '/playlists',
+  .state('app.new', {
+      url: '/new',
       views: {
         'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
+          templateUrl: 'templates/new.html',
+          controller: 'compteCtrl'
         }
       }
-    })
+    });
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/compte');
 });
--- www/js/controllers/compteCtrl.js
+++ www/js/controllers/compteCtrl.js
@@ -1,7 +1,8 @@
-function compteCtrl($scope, $http){
+function compteCtrl($scope, $http, $state){
   $scope.user = {};
   $scope.add = function () {
     var data = $scope.user;
+    $state.go('app.new')
     $http.post('http://localhost:8000/users', data);
   };
 }
